42710156c8d760a40d5d38583fb7fc814c6e97f2
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-06 18:56:55 +0100

    Edit post functionality

--- edit.py
+++ edit.py
@@ -4,11 +4,20 @@ from database import Entry
 import logging
 
 class Edit(Handler):
-    def render_front(self, template, subject="", content="", error=""):
+    def get(self):
         post_id = self.request.query.split('=')[1]
         post = Entry.get_by_id(int(post_id))
         subject = post.subject
         content = post.content
+        self.render('edit.html', subject=subject, content=content, post_id=post_id, error="")
 
-    def get(self):
-        self.render_front('edit.html')
+    def post(self):
+        post_id = self.request.get('post')
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        post = Entry.get_by_id(int(post_id))
+
+        post.subject = subject
+        post.content = content
+        post.put()
+        self.redirect('/blog/' + post_id)
