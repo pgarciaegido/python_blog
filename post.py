486fe678b540ef7ec71b92978663d2b468bda0f2
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-07 17:16:11 +0100

    Styling comments

--- post.py
+++ post.py
@@ -7,5 +7,4 @@ class Post(Handler):
     def get(self, product_id):
         entry    = database.Entry.get_by_id(int(product_id))
         comments = database.Comments.by_entry(int(product_id))
-        logging.info(comments)
         self.render("post.html", entry=entry, comments=comments)
