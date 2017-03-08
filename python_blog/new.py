6150f207c3e52b3014fe50b1093114abc6912098
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-08 14:15:29 +0100

    Create new post security bug fixed

--- python_blog/new.py
+++ python_blog/new.py
@@ -10,30 +10,35 @@ class New(Handler):
                     author=author, error=error)
 
     def get(self):
+        user = self.get_user_cookie()
         # Check cookies to see if user is logged
-        if self.request.cookies.get('userid') == '':
+        if not user:
             self.redirect('/blog/signup')
         else:
             self.render_front()
 
     def post(self):
-        # Get both the subject and the content
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-        author = self.get_user_cookie().username
-        likes = 0
-
-        if subject and content:
-            # If both exist, create new db instance
-            e = database.Entry(subject=subject, content=content,
-                               author=author, likes=likes)
-            # This saves the instance
-            e.put()
-
-            # This gets the (default) id of the entry
-            e_id = str(e.key().id())
-
-            self.redirect('/blog/' + e_id)
+        user = self.get_user_cookie()
+        if not user:
+            self.redirect('/blog/login')
         else:
-            error = "Title and content please"
-            self.render_front(subject, content, error)
+            # Get both the subject and the content
+            subject = self.request.get('subject')
+            content = self.request.get('content')
+            author = user.username
+            likes = 0
+
+            if subject and content:
+                # If both exist, create new db instance
+                e = database.Entry(subject=subject, content=content,
+                                   author=author, likes=likes)
+                # This saves the instance
+                e.put()
+
+                # This gets the (default) id of the entry
+                e_id = str(e.key().id())
+
+                self.redirect('/blog/' + e_id)
+            else:
+                error = "Title and content please"
+                self.render_front(subject, content, error)
